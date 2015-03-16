cc618dd927eed31c32dd8cfef0aa1b6dd16dd8a3
Dean Landolt <dean@deanlandolt.com>
2015-03-15 23:24:22 -0400

    The order test is intentionally failing -- object serialization update should fix it

--- test/order.js
+++ test/order.js
@@ -5,43 +5,53 @@ var typewise = require('typewise');
 var test = require('tape');
 var bops = require('bops');
 
-var example = [
+var expected = [
   null,
-  0.304958230,
   true,
+  0.304958230,
   4,
   'hello',
   [ 'foo', 'bar', 'baz' ],
   { k: 'X', b: false },
   { k: 'X', b: true },
+  { k: 'X', c: false },
+  { k: 'X', c: true },
+  { k: 'X', c: true, d: null },
   { whatever: false },
   { whatever: true }
-].sort(typewise.compare);
+];
+var sorted = expected.sort(typewise.compare);
+
+test('sorts in expected order', function (t) {
+  t.equal(
+    bops.to(bytewise.encode(sorted), 'hex'),
+    bops.to(bytewise.encode(expected), 'hex')
+  );
+  t.end();
+});
 
 test('sorts with same order when encoded', function (t) {
-  var sorted = example
+  var encoded = expected
     .map(bytewise.encode)
     .sort(bytewise.compare)
     .map(bytewise.decode);
 
   t.equal(
-    bops.to(bytewise.encode(sorted),'hex'),
-    bops.to(bytewise.encode(example),'hex')
+    bops.to(bytewise.encode(encoded), 'hex'),
+    bops.to(bytewise.encode(expected), 'hex')
   );
   t.end();
-})
+});
 
 test('sorts with same order when hex encoded', function (t) {
-  var sorted = example
+  var encoded = expected
     .map(bytewiseHex.encode)
     .sort()
     .map(bytewiseHex.decode);
 
   t.equal(
-    bops.to(bytewise.encode(sorted),'hex'),
-    bops.to(bytewise.encode(example),'hex')
+    bops.to(bytewise.encode(encoded), 'hex'),
+    bops.to(bytewise.encode(expected), 'hex')
   );
   t.end();
-})
-
-
+});
